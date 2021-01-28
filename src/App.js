48e804e7c7c1fe2a10792e4c27dab8d5a8f8b703
rafik mami <rafik.mami94@gmail.com>
2021-01-28 21:10:08 +0100

    checkpoint-jsx

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App"  Style={{border:'solid 1px black',maxWidth:'100vw'}}>
+      <h1 className='title red'> Rafik Mami </h1>
+      <br/>
+      <img src={"/imageInSrc.png"} alt="logo"/>
+      <br/>
+      <img src="/imageInPublic.png" alt ='myImage'/>
+      <video width='320' height='240' controls >
+
+      <source src="myVideo.mp4" type="video/mp4"/>
+      </video>
+    
+      </div>  
   );
 }
-
 export default App;
